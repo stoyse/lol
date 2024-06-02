f6e383c7a04f52209083bb8289012ff7f8fe7c54
stoyse <julianfaitdugaming@gmail.com>
2024-06-02 02:52:17 +0200

    jnljnjl

---
+++ menu_button.js
@@ -0,0 +1,8 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+      }
+});
